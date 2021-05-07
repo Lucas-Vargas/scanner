ac796f7bdea1dc49f26e66985ab3240ec1d0fdc4
Lucas da Silva Vargas <lucas.vargas@rede.ulbra.br>
2021-05-07 19:04:05 -0300

    projeto scanner - 07/03/2021

---
+++ src/trabalho1/questão2.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho1;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class questão2 {
+
+   public static void main(String[] args) {
+       
+       Scanner teclado = new Scanner (System.in);
+       int a, b, c;
+       System.out.println("Insira o valor A:");
+       a = teclado.nextInt();
+       System.out.println("Insira o valor B:");
+       b = teclado.nextInt();
+       if (a == b){
+           c = a+b;
+           System.out.println(c);
+       }else{
+           c = a*b;
+           System.out.println(c);
+       }
+               
+       
+       
+               
+       
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+            
+       
+       
+
+               
+       
+               
+
+       
+              
+   }
+
+    
+}
